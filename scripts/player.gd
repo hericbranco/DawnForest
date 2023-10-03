663da95fcb3e7150dcdeb236513a7308dad5d18c
Heric Branco <hericbranco@gmail.com>
2023-10-03 14:39:37 -0300

    feat - add player actions(attack/defending/crouch)

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,10 @@ onready var player_sprite: Sprite = get_node("texture")
 var velocity:Vector2
 var jump_count:int=0
 var landing:bool=false
+var attacking:bool=false
+var defending:bool=false
+var crouching:bool=false
+var can_trak_input:bool=true
 
 export(int) var speed
 export(int) var jump_speed
@@ -14,6 +18,7 @@ export(int) var player_gravity
 func _physics_process(delta:float):
 	horizontal_movement_env()
 	vertical_movement_env()
+	actions_env()
 	gravity(delta)
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -22,15 +27,48 @@ func _physics_process(delta:float):
 	
 func horizontal_movement_env() ->void: 
 	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if can_trak_input == false or attacking:
+		velocity.x=0
+		return
+		
 	velocity.x = input_direction*speed
 
 func vertical_movement_env() ->void: 
 	if is_on_floor():
 		jump_count=0
 		
-	if Input.is_action_just_pressed("ui_select") and jump_count < 2:
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2 and can_trak_input and not attacking:
 		jump_count += 1
 		velocity.y = jump_speed
+
+func actions_env() ->void:
+	attack()
+	crouch()
+	defense()
+
+func attack() ->void:
+	var attack_condition:bool=not attacking and not crouching and not defending
+	if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
+		attacking=true
+		player_sprite.normal_attack=true
+	
+func crouch() ->void:
+	if Input.is_action_pressed("crouch") and is_on_floor() and not defending:
+		crouching=true
+		can_trak_input=false
+	elif not defending:
+		crouching=false
+		can_trak_input=true
+		player_sprite.crouching_off=true
+		
+func defense() ->void:
+	if Input.is_action_pressed("defense") and is_on_floor() and not crouching:
+		defending=true
+		can_trak_input=false
+	elif not crouching:
+		defending=false
+		can_trak_input=true
+		player_sprite.shield_off=true
 	
 func gravity(delta:float) ->void: 
 	velocity.y += delta * player_gravity
--- scripts/player/player_texture.gd
+++ scripts/player/player_texture.gd
@@ -1,13 +1,19 @@
 extends Sprite
 class_name playerTexture
 
+var suffix:String="_right"
+var normal_attack:bool=false
+var shield_off:bool=false
+var crouching_off:bool=false
+
 export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
 export(NodePath) onready var ObjPlayer = get_node(ObjPlayer) as KinematicBody2D
 
 func animate(direction:Vector2) -> void:
 	verity_position(direction)
-	
-	if direction.y!=0:
+	if ObjPlayer.attacking or ObjPlayer.defending or ObjPlayer.crouching:
+		action_behavior()
+	elif direction.y!=0:
 		vertical_behavior(direction)
 	elif ObjPlayer.landing == true:
 		animation.play("landing")
@@ -18,8 +24,20 @@ func animate(direction:Vector2) -> void:
 func verity_position(direction:Vector2) -> void:
 	if direction.x > 0:
 		flip_h=false
+		suffix="_right"
 	elif direction.x < 0:
 		flip_h=true
+		suffix="_left"
+
+func action_behavior() ->void:
+	if ObjPlayer.attacking and normal_attack:
+		animation.play("attack"+suffix)
+	elif ObjPlayer.defending and shield_off:
+		animation.play("shield")
+		shield_off=false
+	elif ObjPlayer.crouching and crouching_off:
+		animation.play("crounch")
+		crouching_off=false
 
 func horizontal_behavior(direction:Vector2) -> void:
 	if direction.x != 0:
@@ -39,3 +57,9 @@ func on_animation_finished(anim_name:String):
 		"landing":
 			ObjPlayer.landing=false
 			ObjPlayer.set_physics_process(true)
+		"attack_left":
+			normal_attack=false
+			ObjPlayer.attacking=false
+		"attack_right":
+			normal_attack=false
+			ObjPlayer.attacking=false
